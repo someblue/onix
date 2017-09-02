442831bc941fd3626edbe7a3aaaf238fbd99eea7
someblue <somebule@gmail.com>
2017-09-02 16:41:54 +0800

    remove onix-web-origin

--- onix-web-origin/src/scripts/helper.js
+++
@@ -1,7 +0,0 @@
-function copyToClipboard(content) {
-    var $temp = $("<input>");
-    $("body").append($temp);
-    $temp.val(content).select();
-    document.execCommand("copy");
-    $temp.remove();
-}
